f3a64a48ccfc628b3737d3139ae012625700b9e9
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-06-17 20:40:00 +0530

    final commit

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 
-let Container = document.getElementById("container");
+let Btn_one = document.getElementById("get-category-data");
+let Btn_two = document.getElementById("get-ingredient-data");
 
-async function getdata() {
+
+Btn_one.addEventListener("click", function () {
+    getCategoryData();
+})
+
+async function getCategoryData() {
     try {
-        let res = await fetch("https://www.themealdb.com/api.php?ref=apilist.fun")
+        let res = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
         console.log(res);
 
         let data = await res.json();
         console.log(data);
     }
     catch (error) {
-        console.log(error)
+        console.log("something went wrong")
     }
 }
 
-getdata();
+getCategoryData()
 
+
+Btn_two.addEventListener("click", function () {
+    getIngredientData()
+})
+
+
+async function getIngredientData() {
+    try {
+        let res = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+        console.log(res);
+
+        let data = await res.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.log("something went wrong")
+    }
+}
+
+getIngredientData()
